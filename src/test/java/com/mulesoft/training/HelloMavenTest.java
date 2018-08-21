878f2bd91c04c43907ce9fc034f60f918dd2bf4e
d846590 <Sunshine.Cotejo@team.telstra.com>
2018-08-21 13:38:47 +0800

    updated junit

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -8,7 +8,7 @@ import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
-	/*@Test
+	@Test
 	public void retrieveFlightsAddsAppropriateHeader() throws Exception {
 		MuleEvent event = runFlow("retrieveFlights");
 		String contentType = event.getMessage().getOutboundProperty("Content-Type");
@@ -18,7 +18,7 @@ public class HelloMavenTest extends FunctionalTestCase {
 	@Test
 	public void mavenFlowReturnsHelloMaven() throws Exception {
 		runFlowAndExpect("mavenFlow", "Hello Maven");
-	}*/
+	}
 
 	@Override
 	protected String getConfigFile() {
